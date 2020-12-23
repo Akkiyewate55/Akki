778c50e54ea9c9c81a9066864af178b7847b8e05
Akki <akshay.yewate12345@gmail.com>
2020-12-23 20:23:42 +0530

    Added

---
+++ Java programe'/Hello.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+class Hello
+{
+   public static void main(String[] arg)
+    {
+        Scanner s=new Scanner(System.in);
+
+
+       System.out.println("Enter name");
+       String name=s.next();
+       System.out.println("Enter age");
+      int b=s.nextInt();
+      System.out.println("Enter no");
+      long p=s.nextLong();
+      System.out.println("Enter name:"+name);
+      System.out.println("Enter age:"+b);
+      System.out.println("Enter no:"+p);
+
+   } 
+}
